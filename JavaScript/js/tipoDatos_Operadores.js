4db51868581e96ff5a2a5284f00da4d8499d0cca
hector.juarez-trabajo <hector.coyotl.juarez@hotmail.com>
2023-05-15 15:35:08 -0600

    Tipo de daros y Operadores

---
+++ JavaScript/js/tipoDatos_Operadores.js
@@ -0,0 +1,45 @@
+'use strict'
+// Tipo de datos y operadores
+
+//Operadores
+var num1 = 7;
+var num2 = 12;
+var operacionSum = num1 + num2;
+var operacionResta = num1 - num2;
+var operacionMul = num1 * num2;
+var operaciondiv = num1 / num2;
+var operacionres = num1 % num2;
+
+
+// alert("El resultado de la Resta es :" + ' ' + operacionResta);
+// alert("El resultado de la Multiplicacion es :" + ' ' + operacionMul);
+// alert("El resultado de la divicion es :" + ' ' + operaciondiv);
+// alert("El resultado de la suma es :" + ' ' + operacionSum);
+// alert("El resuido es  :" + ' ' + operacionres);
+
+
+
+// Tipo de datos 
+
+var numero_entero = 44 ;
+var cadena_text = "hola que tal";
+var verdadero_o_falso =true;
+
+var numero_falso = "33.4";
+
+Number();
+
+console.log(parseFloat(numero_falso));
+console.log(parseInt(numero_falso));
+console.log(Number(numero_falso));
+
+
+console.log(String(numero_falso)+4);
+
+
+// para saber el tipo de datos
+
+
+console.log(typeof numero_entero);
+console.log(typeof numero_falso);
+console.log(typeof verdadero_o_falso);
