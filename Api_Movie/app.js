5f97798a43007afbbbabd736b3c66a377803d512
hector.juarez-trabajo <hector.coyotl.juarez@hotmail.com>
2023-06-12 17:22:56 -0600

    Trabajo api

--- Api_Movie/app.js
+++ Api_Movie/app.js
@@ -1,9 +1,7 @@
 "use strict";
-
 let pagina =1;
 const btnAnterior =document.getElementById('btnAnterior');
 const btnSiguiente =document.getElementById('btnSiguiente');
-
 
 btnSiguiente.addEventListener('click',()=>{
     if(pagina < 1000){
@@ -21,35 +19,21 @@ btnAnterior.addEventListener('click',()=>{
     
 });
 
-
-
 const cargarPeliculas = async () => {
-
     try {
         const  respuesta = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=bc1fb49962e7f15fafd12008c6392575&language=es-MX&page=${pagina}`);
-        console.log(respuesta);
-
-
         if (respuesta.status === 200) {
         const datos = await respuesta.json();
-
         let peliculas ='';
-
         datos.results.forEach(pelicula => {
             peliculas +=`
-            
             <div class="pelicula">
             <img class="poster" src="https://image.tmdb.org/t/p/w500/${pelicula.poster_path}">
-     
             <h3 class="titulo">${pelicula.title}</h3>
             </div>
-            
             `;
-
         });
-
         document.getElementById('contenedor').innerHTML =peliculas;
-            
         }else if(respuesta.status === 401){
             console.log('llave incorrecta');
         }
@@ -59,13 +43,9 @@ const cargarPeliculas = async () => {
             console.log('Error');
         }
 
-        
-
     } catch (error) {
         console.log(error);
     }
-
-  
 };
 
 cargarPeliculas();
