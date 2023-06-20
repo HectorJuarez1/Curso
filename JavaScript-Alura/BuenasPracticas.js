51ad5861892181d7fb594e7768074035560f204f
hector.coyotl-casa <hector.coyotl.juarez@hotmail.com>
2023-06-20 11:55:52 -0600

    JavaScript

---
+++ JavaScript-Alura/BuenasPracticas.js
@@ -0,0 +1,8 @@
+let nombreEmpleado = "Jose Perez";
+let NombreEmpleado = "Pedro Ramirez";
+
+
+
+let edadEmpleado = 40;
+console.log(nombreEmpleado);
+consolo.log(NombreEmpleado);
---
+++ JavaScript-Alura/ConversionTipos.js
@@ -0,0 +1,16 @@
+//Tipo de datos 
+
+let nombrePasajero = "Pedro Silva";
+console.log(nombrePasajero);
+
+
+let valorBoleto = 1650;
+const tasaEmbarque = 60;
+var getionAgencia = 120.55;
+
+
+let boletoActivo = true;
+console.log(boletoActivo);
+
+let totalBoletos = valorBoleto +tasaEmbarque+getionAgencia;
+console.log(totalBoletos);
