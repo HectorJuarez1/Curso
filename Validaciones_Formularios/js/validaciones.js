ba534d05999045fa0f962c1e721b7ed7803ca743
hector.juarez-trabajo <hector.coyotl.juarez@hotmail.com>
2023-06-28 21:08:29 -0600

    fechas

---
+++ Validaciones_Formularios/js/validaciones.js
@@ -0,0 +1,17 @@
+const inputNacimiento = document.querySelector("#birth");
+inputNacimiento.addEventListener("blur",(evento)=>{
+    validarNacimiento(evento.target);
+});
+
+
+function validarNacimiento(input){
+    const fechaCliente = new Date(input.value);
+    mayorDeEdad(fechaCliente);
+}
+
+
+function mayorDeEdad(fecha){
+    const fechaActual = new Date();
+    const diferenciaFechas = new Date(fecha.getUTCFullYear()+18,fecha.getUTCMonth(),fecha.getUTCFullDate());
+    console.log(diferenciaFechas);
+}
